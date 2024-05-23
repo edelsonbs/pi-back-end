e38ff9d0b81edfbd1f7171f8bf3cb116977b8ab9
Feh Dev <fehdev01@outlook.com>
2024-05-23 18:40:44 -0300

    modifiquei a Rota da API

--- app.js
+++ app.js
@@ -1,25 +1,28 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const rotasProduto = require('./src/routes/rotasProduto');
-const rotasSupermercado = require('./src/routes/rotasSupermercado');
-const environments = require('./src/config/environments');
+const environments = require("./src/config/environments");
+const rotas = require("./src/routes/rotas");
 
 app.use(express.json());
 
 // Rotas da API
-app.use('/api/produtos', rotasProduto);
-app.use('/api/supermercados', rotasSupermercado);
+app.use("/api", rotas);
+
+// app.use('/api/produtos', rotasProduto);
+// app.use('/api/supermercados', rotasSupermercado);
 
 const PORTA = environments.PORTA;
 
-
 (async () => {
   try {
+    app.get("/", (req, res) => {
+      res.send(`Aqui deu certo Carai`);
+    });
 
     app.listen(PORTA, () => {
       console.log(`Servidor rodando em http://localhost:${PORTA}`);
     });
   } catch (error) {
-    console.error('Erro ao sincronizar o banco de dados:', error);
+    console.error("Erro ao sincronizar o banco de dados:", error);
   }
 })();
